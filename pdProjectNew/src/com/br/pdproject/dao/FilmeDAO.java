a086ce571cfc974329136a47d27d26b20cccaaaf
Lucas Carvalho <lucasprocopio.carvalho@gmail.com>
2016-09-11 12:47:49 -0300

    Commit alterações para corrigir a Inserção de filmes.

--- pdProjectNew/src/com/br/pdproject/dao/FilmeDAO.java
+++ pdProjectNew/src/com/br/pdproject/dao/FilmeDAO.java
@@ -5,7 +5,10 @@
  */
 package com.br.pdproject.dao;
 
+import com.br.pdproject.dominio.Ator;
 import com.br.pdproject.dominio.Filme;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
@@ -34,6 +37,21 @@ public class FilmeDAO extends GenericDAO {
         EntityManager em = GenericDAO.getEntityManager().createEntityManager();
         try{    
             em.getTransaction().begin();
+            //Preenchimento dos campos não definidos pelo usuário do sistema.
+            filme.setAnoLancamento(1990);
+            filme.setDuracaoAluguel(2);
+            filme.setTaxaAluguel(4.0);
+            filme.setDuracao(120);
+            filme.setCustoReposicao(10.00);
+            Ator ator1 = new Ator();
+            Ator ator2 = new Ator();
+            List<Ator> atores = new ArrayList<Ator>();
+            ator1.setId(165);
+            ator2.setId(125);
+            atores.add(ator1);
+            atores.add(ator2);
+            filme.setAtores(atores);
+            
             em.persist(filme);
             em.getTransaction().commit();
         }catch(Exception e){
